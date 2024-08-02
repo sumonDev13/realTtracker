73dadb8ab93da83e2d5470a1ae07638d08c49c03
sumonDev13 <sumonmondal1530765@gmail.com>
2024-08-03 02:56:15 +0600

    setting the map

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
 
 io.on("connection", function (socket) {
-  console.log("connected to socket");
+    socket.on("send-location", function(data){
+    io.emit("receive-location", {id:socket.id, ...data});    
+    })
+    console.log("connected to socket");
 });
 
 app.get("/", function (req, res) {
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,31 @@
 const socket = io();
+
+if(navigator.geolocation) {
+    navigator.geolocation.watchPosition((position)=>{
+        const {latitude,longitude} = position.coords;
+        socket.emit("send-location",{latitude:latitude,longitude:longitude})
+    },(error)=>{
+        console.log(error)
+    },
+    {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000
+    }
+)
+}
+
+const map = L.map("map").setView([0,0],10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        attribution: "sumonDev@13"
+    }
+).addTo(map);
+
+const markers = {}
+
+socket.on("receive-location",(data)=>{
+    const {id,latitude,longitude} = data;
+    map.setView([latitude,longitude]);
+})
