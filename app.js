266acf51affd19b79cd1197e947db0792eefab80
sumonDev13 <sumonmondal1530765@gmail.com>
2024-08-03 02:19:17 +0600

    socketio connected

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-import { Server } from 'socket.io';
-import express from 'express';
-import { createServer } from 'http';
+import { Server } from "socket.io";
+import express from "express";
+import { createServer } from "http";
+
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename,);
-
-
+const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
-
-io.on("connection", function(socket) {
-    console.log("connected")
-})
-
-
-app.get('/', function(req, res) {
-    res.render('index')
+io.on("connection", function (socket) {
+  console.log("connected to socket");
 });
 
-app.listen(3000);
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+const port = 3000;
+server.listen(port, () => console.log(`Listening on port ${port}`));
